3b3191ebb1043509a3b34b4079e872b86fc7e73a
devmanoj777 <devmanoj777@gmail.com>
2022-12-31 09:31:04 +0530

    done

--- ui.py
+++ ui.py
@@ -1,3 +1,28 @@
 import tkinter as tk 
-window=tk.Tk()
-window.mainloop()
+from tkinter import ttk
+from tkinter.messagebox import showinfo
+
+w=tk.Tk()
+
+w.geometry('800x600+50+50')
+w.title('dev app')
+w.attributes('-topmost',1)
+
+
+def download_clicked():
+    showinfo(
+        title='information',
+        message='dowloaded successfully'
+    )
+
+
+lable=ttk.Label(w,text='Welcome to tk-inter').pack()
+lable=ttk.Label(w,text='login to your app').pack()
+
+btn_download=ttk.Button(w,text='Download',command=download_clicked).pack()
+btn=ttk.Button(w,text='exit',command=lambda:w.quit())
+
+
+btn.pack(ipadx='5',ipady='5',expand=True)
+
+w.mainloop()
